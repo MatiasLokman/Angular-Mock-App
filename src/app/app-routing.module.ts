41f42b288ed4ed194fbdba8638188470a34b4ee7
Matías Lokman <matias.lokman@gmail.com>
2021-10-28 10:23:27 -0300

    Routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () =>
+      import('./pages/inicio/inicio.module').then((m) => m.InicioModule),
+  },
+  {
+    path: 'productos',
+    loadChildren: () =>
+      import('./pages/productos/productos.module').then(
+        (m) => m.ProductosModule
+      ),
+  },
+  {
+    path: 'atencion-al-cliente',
+    loadChildren: () =>
+      import('./pages/atencion-al-cliente/atencion-al-cliente.module').then(
+        (m) => m.AtencionAlClienteModule
+      ),
+  },
+  {
+    path: 'carrito',
+    loadChildren: () =>
+      import('./pages/carrito/carrito.module').then((m) => m.CarritoModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'inicio',
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/atencion-al-cliente/atencion-al-cliente-routing.module.ts
+++ src/app/pages/atencion-al-cliente/atencion-al-cliente-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AtencionAlClienteComponent } from './components/atencion-al-cliente/atencion-al-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AtencionAlClienteComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AtencionAlClienteRoutingModule { }
+export class AtencionAlClienteRoutingModule {}
