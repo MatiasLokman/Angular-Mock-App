bdd6c75f8aa94314cd0eeb7d013a8c11a3b9bc4a
Matías Lokman <matias.lokman@gmail.com>
2021-10-28 10:10:01 -0300

    Angular material added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+
+let materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatTableModule,
+];
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, materialModules],
+  exports: [materialModules],
 })
 export class MaterialModule {}
