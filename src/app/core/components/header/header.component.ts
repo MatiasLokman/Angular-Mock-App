148617486f5f17139b92af7f97e720633341dba2
Matías Lokman <matias.lokman@gmail.com>
2021-10-28 13:26:37 -0300

    Header made

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  links = [
+    {
+      path: 'inicio',
+      desc: 'HOME',
+      isIcon: false,
+    },
+    {
+      path: 'productos',
+      desc: 'PRODUCTOS',
+      isIcon: false,
+    },
+    {
+      path: 'atencion-al-cliente',
+      desc: 'CONTACTO',
+      isIcon: false,
+    },
+    {
+      path: 'carrito',
+      desc: 'shopping_cart',
+      isIcon: true,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
