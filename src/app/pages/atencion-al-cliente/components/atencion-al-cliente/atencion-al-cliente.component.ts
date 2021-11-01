ae75a28eaadf18983494d96711a96b289a9cedb6
Matías Lokman <matias.lokman@gmail.com>
2021-11-01 13:54:57 -0300

    Styling and restoring values ​​when submitting and resetting form data

--- src/app/pages/atencion-al-cliente/components/atencion-al-cliente/atencion-al-cliente.component.ts
+++ src/app/pages/atencion-al-cliente/components/atencion-al-cliente/atencion-al-cliente.component.ts
@@ -3,6 +3,7 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  RequiredValidator,
   Validators,
 } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -37,21 +38,77 @@ export class AtencionAlClienteComponent implements OnInit {
   ngOnInit(): void {}
 
   submitForm() {
-    if (this.formData.valid) {
-      this.loading = true;
-      setTimeout(() => {
-        if (this.formData.valid) console.log(this.formData.value);
-        this.loading = false;
+    this.loading = true;
+    setTimeout(() => {
+      if (this.formData.valid) console.log(this.formData.value);
+      this.loading = false;
+      this._snackBar.open('Formulario enviado con éxito', 'x', {
+        duration: 3000,
+      });
+    }, 3000);
 
-        this._snackBar.open('Formulario enviado con éxito', 'x', {
-          duration: 3000,
-        });
-      }, 3000);
-      this.formData.reset();
-    }
+    // I need to save the data inside a object before setting the values to null, in order not to lose them
+    this.formData.setValue({
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+      link: '',
+      genre: '',
+      newsletter: '',
+    });
+
+    this.formData.get('name')?.clearValidators();
+    this.formData.get('name')?.updateValueAndValidity();
+    this.formData.get('name')?.addValidators(Validators.required);
+
+    this.formData.get('email')?.clearValidators();
+    this.formData.get('email')?.updateValueAndValidity();
+    this.formData
+      .get('email')
+      ?.addValidators([Validators.required, Validators.email]);
+
+    this.formData.get('message')?.clearValidators();
+    this.formData.get('message')?.updateValueAndValidity();
+    this.formData
+      .get('message')
+      ?.addValidators([
+        Validators.required,
+        Validators.minLength(20),
+        Validators.maxLength(350),
+      ]);
   }
 
   resetForm() {
-    this.formData.reset();
+    // this.formData.reset();
+    this.formData.setValue({
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+      link: '',
+      genre: '',
+      newsletter: '',
+    });
+
+    this.formData.get('name')?.clearValidators();
+    this.formData.get('name')?.updateValueAndValidity();
+    this.formData.get('name')?.addValidators(Validators.required);
+
+    this.formData.get('email')?.clearValidators();
+    this.formData.get('email')?.updateValueAndValidity();
+    this.formData
+      .get('email')
+      ?.addValidators([Validators.required, Validators.email]);
+
+    this.formData.get('message')?.clearValidators();
+    this.formData.get('message')?.updateValueAndValidity();
+    this.formData
+      .get('message')
+      ?.addValidators([
+        Validators.required,
+        Validators.minLength(20),
+        Validators.maxLength(350),
+      ]);
   }
 }
